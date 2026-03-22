eb32b98c61360960ae0810962cc7c282044a5f6d
shubham-dethe <shubhamdethe88@gmail.com>
2026-03-22 18:55:25 +0530

    Change the return type to response entity

--- src/main/java/com/example/controller/GitController.java
+++ src/main/java/com/example/controller/GitController.java
@@ -3,6 +3,8 @@ package com.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,9 +24,11 @@ public class GitController {
 	GitService service;
 	
 	@PostMapping
-	public void addData(@RequestBody RequestGitDto requestGitDto) {
+	ResponseEntity<String> addData(@RequestBody RequestGitDto requestGitDto) {
 		
 		service.addGit(requestGitDto);
+		
+		return new ResponseEntity<String>("Added the Data", HttpStatus.CREATED);
 		
 	}
 	
